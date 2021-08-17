df738236195834f6b948286150503cc34d047978
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-08-17 18:33:12 -0300

    variables y operadores

---
+++ js/holaMundo.js
@@ -0,0 +1,30 @@
+//esto es un comentario simple
+/*esto es un comentario de 
+multiples lineas*/
+
+//Mostrar un mensaje por Consola
+
+console.log("Hola mundo");
+
+//mostrar un mensaje en la pagina web
+document.write("Este es mi primer mensaje");
+
+//mostrar una ventana emergente
+//alert("aler1");
+//crear variables var,let const
+
+let palabraBuscada ="hola";
+document.write("<br> variable palabra Buscada : " + palabraBuscada);
+
+//modificar el valor de una variable
+palabraBuscada="pagina de blog";
+document.write("<br> variable palabra Buscada : " + palabraBuscada);
+
+//variable const
+const comision = 17;
+document.write("<br> constante comision  : " + comision);
+//variables bolean
+let luz = false;
+//incrementar un valor con ++
+let suma = 20;
+console.log(++suma); //con resta es igual pero con el signo de --
