5599d6c6d40018ec22f069150405d01e052deb3d
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-08-18 19:29:14 -0300

    Ejercicios Resueltos 1-5

---
+++ js/EJercicios1-5.js
@@ -0,0 +1,28 @@
+/*1.- Escribe un programa de una sola línea que haga que aparezca en la
+pantalla un alert que diga “un mensaje”*/
+//alert("Hola");
+
+/*2.- Escribe un programa de una sola línea que escriba en la pantalla un texto
+que diga «Hello World» (document.write).*/
+//document.write("Hello World");
+
+/*3.- Escribe un programa de una sola línea que escriba en la pantalla el
+resultado de sumar 3 + 5.*/
+/*
+let numero1 = 5;
+let numero2= 3;
+document.write(numero1+numero2);*/
+
+/*4.- Escribe un programa de dos líneas que pida el nombre del usuario con un
+prompt y escriba un texto que diga «Hola nombreUsuario»*/
+/*
+let nombre = prompt("ingresa tu nombre : ");
+document.write("Hola: "+nombre);*/
+
+/*5.- Escribe un programa de tres líneas que pida un número, pida otro número
+y escriba el resultado de sumar estos dos números.*/
+/*
+let numero1=parseInt(prompt("ingrese el primer numero : "));
+let numero2=parseInt(prompt("ingrese el segundo numero: "));
+document.write(numero1 + numero2);
+*/
