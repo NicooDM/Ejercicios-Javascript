6a5b84f48004d4ba61770821e19459677086db71
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-08-19 17:35:29 -0300

    Se agrego el archivo condicionales.js

---
+++ js/estructurasCondicional.js
@@ -0,0 +1,22 @@
+/* 
+if(condicion logica)
+{
+    codigo a ejecutar si se cumple la condicion
+}
+else{
+    codigo a ejecutar si no se cumple la condicion
+}
+*/
+/*
+let numero1= parseInt(prompt("Ingrese el primer numero : "));
+let numero2= parseInt(prompt("Ingrese el primer numero : "));
+
+if(numero1 > numero2){document.write("el numero mayor es : " + numero1)}*/
+/*else (
+    if(numero2>numero1){
+        document.write("el numero mayor es : "+numero2)
+    }
+    else{
+        document.write("los numeros son iguales")
+    }
+)*/
