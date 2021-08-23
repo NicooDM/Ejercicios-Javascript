95f499b08b0f376059b5fe448b2abf91106f25a4
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-08-23 14:00:33 -0300

    EJercicios terminados 6-11

---
+++ js/bucles.js
@@ -0,0 +1,34 @@
+/*Bucles repetitivos */
+
+//while
+/* 
+while(condicionlogica){
+    codigo a ejecutar
+}
+*/
+//ejecutar una linea de codigo 100 veces
+/*let contador = 1;
+
+while(contador >=100){
+    console.log("Elemento : "+contador+"<br>");
+    contador++;
+
+}*/
+//do-while hacer...mientras (se cumple la condicion)
+/*
+let elemento = 1;
+
+
+do{
+    
+    document.write("Elemento : "+elemento+"<br>");
+    elemento++;
+}while(elemento<=100)
+*/
+//Estructura for
+/*
+for(crear la variable; condicion;incremento o decremento la variable){
+    codigo a ejecutar
+
+}
+ */
