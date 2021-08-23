62f60b893cd93a393bd2cbf38b598645a2d1da73
Jose Nicolas Di Marco <70348348+NicoyRom@users.noreply.github.com>
2021-08-23 14:17:08 -0300

    Add files via upload

---
+++ js/Ejercicios6-11.js
@@ -0,0 +1,59 @@
+/*
+6
+Escribe un programa que pida dos números y escriba en la pantalla cual
+es el mayor.
+ */
+/*
+let numero1 = parseInt(prompt("Ingrese el primer numero : "));
+let numero2= parseInt(prompt("Ingrese el segundo numero : "));
+
+if(numero1>numero2){
+    console.log("el numero mayor es : " +numero1);
+
+}
+else if (numero2>numero1){
+    console.log("el numero mayor es : "+numero2)
+}
+if(numero1===numero2){
+    console.log("Los numeros son iguales")
+}*/
+/*
+7- Escribe un programa que pida 3 números y escriba en la pantalla el mayor
+de los tres.
+ */
+/*
+let numero1 = parseInt(prompt("Ingrese el primer numero : "));
+let numero2= parseInt(prompt("Ingrese el segundo numero : "));
+let numero3 = parseInt(prompt("Ingrese el tercer numero : "));
+
+if(numero1>numero2 && numero1>3){
+    console.log("El numero mayor es : "+numero1);
+}
+else if(numero2>numero1 && numero2>numero3){
+    console.log("El numero mayor es : "+numero2);
+}
+if(numero3>numero1 && numero3>numero2){
+    console.log("El numero mayor es : "+numero3);
+}
+else if(numero1===numero2&& numero1===numero3){
+    console.log("Los numeros son iguales");
+}
+*/
+/*
+8.- Escribe un programa que pida un número y diga si es divisible por 2
+*/ 
+/*
+let numero= parseInt(prompt("Ingrese el numero : "));
+if(numero%2==0){
+    console.log("El numero es divisible por 2");
+}
+else(console.log("El numero no es divisible por 2"));*/
+
+/*10.-11 Escribe un programa que pida un número y nos diga si es divisible por 2,
+3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro) */
+/*
+let numero= parseInt(prompt("Ingrese el numero : "));
+if(numero%2===0){console.log("El numero es divisible por 2");}
+else if(numero%3===0){console.log("El numero es divisible por 3");}
+if(numero%5===0){console.log("El numero es divisible por 5");}
+else if(numero%7===0){console.log("El numero es divisible por 7");}*/
