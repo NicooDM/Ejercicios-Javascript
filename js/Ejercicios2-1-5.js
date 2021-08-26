ddb1e7046a10b135d2c7b09a7ddb3ab280491b50
Nicolas Di Marco <dimarconicolas5@gmail.com>
2021-08-26 14:33:02 -0300

    Agregado ejercicios y cambios en archivos js

--- js/Ejercicios2-1-5.js
+++ js/Ejercicios2-1-5.js
@@ -22,7 +22,66 @@ Si ingreso un numero que no esté dentro del rango de 0 a 10 mostrar un mensaje 
 “número erróneo”. Si el numero ingresado no es válido mostrar el mensaje “Introduce un
 número válido”.
  */
+/*
+let nota = parseInt(prompt("Ingrese la nota del alumno"));
+
+if(isNaN(nota)){
+   nota= parseInt(prompt("Ingrese la nota CORRECTA del alumno"));
+
+}
+
+switch(nota){
+    case 0:
+    case 1:
+    case 2:
+        document.write("NOTA MUY DEFICIENTE");
+        break;
+        case 3:
+        case 4:
+            document.write("NOTA INSUFICIENTE");
+            break;
+            case 5:
+            case 6:
+                document.write("NOTA SUFICIENTE");
+                break;
+                case 7:
+                    document.write("NOTA BIEN");
+                    case 8:
+                    case 9:
+                        document.write("NOTA NOTABLE");
+                        break;
+                        case 10:
+                            document.write("NOTA EXCELENTE");
+                            break;
+                       
+}
+*/
+/*
 
 
+/*
+4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número
+deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá
+indicarse la suma total de los números introducidos.
+ */
+/*
+let suma = 0;
 
+do{
+    let numero=prompt("Ingrese los numeros");
+
+    if(Number(numero)==numero){
+        numero=Number(numero);
+        suma = numero + suma;
+    }
+    else{
+        if(numero!=undefined){
+            alert(numero + "NO es un numero");
+        }
+    }
+  
+
+}while(numero!=undefined);
+
+document.write(suma);*/
 
